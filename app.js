e9997702c518c1403934f1ca98603b54be0e489d
Derek <derekdanielson84@gmail.com>
2023-10-10 11:01:47 -0600

    Cleaned up notes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const inputFieldEl = document.getElementById('result');
 const buttonsEl = document.querySelectorAll('button');
 
-
+//Loop through buttons to get each individual button
 for (let i = 0; i < buttonsEl.length; i++){
+    //Event listener for each button
     buttonsEl[i].addEventListener('click', ()=>{
         const buttonValue = buttonsEl[i].textContent;
+        //Functions to run for 'clear', calculate, and display values
         if (buttonValue === 'C'){
             clearResult();
         } else if (buttonValue === '='){
@@ -15,14 +17,17 @@ for (let i = 0; i < buttonsEl.length; i++){
     });
 };
 
+//Clear input field
 function clearResult(){
     inputFieldEl.value = '';    
 };
 
+//Calculate results
 function calculateResult(){
     inputFieldEl.value = eval(inputFieldEl.value);
 };
 
+//Display values
 function appendValue(buttonValue){
     inputFieldEl.value += buttonValue;
 };
