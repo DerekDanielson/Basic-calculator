93e44cc46e08c302ab1e574ea7fbacd0caf7179b
Derek <derekdanielson84@gmail.com>
2023-10-10 10:57:43 -0600

    Cleaned up classes

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const inputFieldEl = document.getElementById('result');
 const buttonsEl = document.querySelectorAll('button');
-const clearBtn = document.querySelector('.clear');
-const operatorBtn = document.querySelectorAll('.operator');
-const numberBtn = document.querySelectorAll('.number');
-const equalsBtn = document.querySelector('.equals');
-const decimalBtn = document.querySelector('.decimal');
 
 
 for (let i = 0; i < buttonsEl.length; i++){
